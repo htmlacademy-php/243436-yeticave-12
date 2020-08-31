5f091d3ad960f943d47c47f61b770bc25c59e875
Pavel Trofimov <pavel.trofimov.tot@mail.ru>
2020-09-01 00:49:44 +0700

    Изменил условия добавления имени класса

--- templates/main.php
+++ templates/main.php
@@ -30,7 +30,7 @@
                                 <span class="lot__amount">Стартовая цена</span>
                                 <span class="lot__cost"><?= htmlspecialchars(get_sum($value['price'])); ?></span>
                             </div>
-                            <div class="lot__timer timer <? if ((floor((strtotime($value['time']) - time()) / 3600)) < 60) : ?> timer--finishing <? endif;?>">
+                            <div class="lot__timer timer <? if ((floor((strtotime($value['time']) - time()) / 3600)) == 0) : ?> timer--finishing <? endif;?>">
                                 <? 
                                     foreach(get_dt_range($value['time']) as $val) {
                                         echo $val;        

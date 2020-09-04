d8c6727496ed5275847728cd3e3b586ba6a0df60
Pavel Trofimov <pavel.trofimov.tot@mail.ru>
2020-09-04 14:45:02 +0700

    Внес правки

--- schema.sql
+++ schema.sql
@@ -4,40 +4,39 @@ USE yeticave;
 
 CREATE TABLE category (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	category_name CHAR(255) NOT NULL,
-	category_code CHAR(255) NOT NULL
+	name VARCHAR(100) NOT NULL,
+	code VARCHAR(20) NOT NULL
 );
 
-INSERT INTO category (category_name, category_code) VALUES ('Доски и лыжи', 'boards'), ('Крепления', 'attachment'), ('Ботинки', 'boots'), ('Одежда', 'clothing');
+INSERT INTO category (name, code) VALUES ('Доски и лыжи', 'boards'), ('Крепления', 'attachment'), ('Ботинки', 'boots'), ('Одежда', 'clothing');
 
 CREATE TABLE lot (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  date_start DATETIME,
-  title CHAR(255) NOT NULL,
-  descruption TEXT NOT NULL,
-  path CHAR(255) NOT NULL,
-  cost INT UNSIGNED,
-  date_finish DATETIME,
-  rate_stap INT UNSIGNED,   
-  users_id INT UNSIGNED,
-  category_id INT UNSIGNED	
+  date_start DATETIME NOT NULL,
+  title VARCHAR(700) NOT NULL,
+  description TEXT NOT NULL,
+  path VARCHAR(100) NOT NULL,
+  cost INT UNSIGNED NOT NULL,
+  date_finish DATETIME NOT NULL,
+  rate_step INT UNSIGNED NOT NULL,   
+  user_id INT UNSIGNED NOT NULL,
+  winner_id INT UNSIGNED,
+  category_id INT UNSIGNED	NOT NULL
 );
 
 CREATE TABLE rate (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	date_rate DATETIME,
-	cost_rate INT UNSIGNED,
-  users_id INT UNSIGNED,
-  lot_id INT UNSIGNED
+	date TIMESTAMP NOT NULL,
+	cost INT UNSIGNED NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  lot_id INT UNSIGNED NOT NULL
 );
 
-CREATE TABLE users (
+CREATE TABLE user (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	date_reg DATETIME,
-  email CHAR(255) NOT NULL,
-	name CHAR(255) NOT NULL,	
+	created_at TIMESTAMP NOT NULL,
+  email VARCHAR(100) NOT NULL,
+	name CHAR(100) NOT NULL,	
 	password CHAR(64) NOT NULL,
-	contact CHAR(255) NOT NULL,
-  lot_id INT UNSIGNED,
-  rate_id INT UNSIGNED
+	contact VARCHAR(20) NOT NULL
 );

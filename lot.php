50483e0e2edfc06e29a6d2b8b09b4e9a138f5f6b
Pavel Trofimov <pavel.trofimov.tot@mail.ru>
2020-09-13 18:02:17 +0700

    Внес правки

--- lot.php
+++ lot.php
@@ -6,6 +6,7 @@
   
   $user_name = 'Павел';
 
+  
   $connect = db_connect('localhost', 'root', 'root', 'yeticave');
 
   if (isset($_GET['id'])) {
@@ -63,34 +64,6 @@
   }
 
   $categories = mysqli_fetch_all($result_categories, MYSQLI_ASSOC);
-
-
-
-  function get_dt_range ($date) {
-    $future_time = strtotime($date);
-    $now_time = time();
-    $result_time_hour = floor(($future_time - $now_time) / 3600);
-    $result_time_min = floor((($future_time - $now_time) % 3600)/60);
-  
-    $result_time_min = str_pad($result_time_min, 2, "0", STR_PAD_LEFT);;
-    $result_time_hour = str_pad($result_time_hour, 2, "0", STR_PAD_LEFT);;
-  
-    return [$result_time_hour, $result_time_min];
-  };
-
-
-
-  function get_sum ($cost) {
-	
-    ceil($cost);
-  
-    if ($cost >= 1000) {
-      $cost = number_format($cost, 0, ',', ' ');
-    }
-    
-    return $cost.' ₽';
-  
-  };
 
 
   $page_content = include_template('lots.php', ['categories' => $categories, 'lots' => $lots]);
